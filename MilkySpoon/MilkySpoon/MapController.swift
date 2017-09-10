ccaafaf8d8aa2a54436d28d414b3ad4eea5a54f8
tomokinteruyuki <teru.sms01@gmail.com>
2017-09-10 14:36:52 +0900

    画像のキャッシュ機能・アニメーション・ログ出力・ナビゲーションスライダーの外部ライブラリを導入

---
+++ MilkySpoon/MilkySpoon/MapController.swift
@@ -0,0 +1,19 @@
+//
+//  MapContrller.swift
+//  MilkySpoon
+//
+//  Created by 友金輝幸 on 2017/09/10.
+//  Copyright © 2017年 MilkySpoon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MapController: UIViewController {
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    
+}
