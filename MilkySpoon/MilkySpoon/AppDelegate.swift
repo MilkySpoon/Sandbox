3b4bffbf8f0811288ad89dcee423b04f20d15b67
tomokinteruyuki <teru.sms01@gmail.com>
2017-09-10 14:53:21 +0900

    GoogleMapの初期設定終了

--- MilkySpoon/MilkySpoon/AppDelegate.swift
+++ MilkySpoon/MilkySpoon/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyBREfc_kcpHMVccZys4IVBt-8w4lRZOZhI")
         return true
     }
 
