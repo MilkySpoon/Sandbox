8268f34e1c9f785a8806b43c396392b98f562bb1
tomokinteruyuki <teru.sms01@gmail.com>
2017-09-10 17:21:26 +0900

    以下を実装 Google mapの表示 タブバーで画面遷移 タブバークリック時のアニメーション

--- MilkySpoon/Pods/Target Support Files/Pods-MilkySpoon/Pods-MilkySpoon-frameworks.sh
+++ MilkySpoon/Pods/Target Support Files/Pods-MilkySpoon/Pods-MilkySpoon-frameworks.sh
@@ -100,6 +100,21 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AlamofireImage/AlamofireImage.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Log/Log.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RAMAnimatedTabBarController/RAMAnimatedTabBarController.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Spring/Spring.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AlamofireImage/AlamofireImage.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Log/Log.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RAMAnimatedTabBarController/RAMAnimatedTabBarController.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Spring/Spring.framework"
+fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
